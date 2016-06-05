68218fbd9d1c7d520dba871ca68dd1bc05abb8a0
samutamm <samutamm@gmail.com>
2016-06-05 16:52:23 +0200

    Congigure API key, but ignore from git.

--- app/src/main/java/com/samutamm/nano/popularmovies/MainActivity.java
+++ app/src/main/java/com/samutamm/nano/popularmovies/MainActivity.java
@@ -3,6 +3,8 @@ package com.samutamm.nano.popularmovies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.squareup.picasso.Picasso;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
