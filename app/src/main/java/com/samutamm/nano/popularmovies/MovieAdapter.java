a1e77be6371d1bd352d3b13fd580328577ed6713
samutamm <samutamm@gmail.com>
2016-06-08 21:47:11 +0200

    Basic image downloading. Still some issues.

--- app/src/main/java/com/samutamm/nano/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/samutamm/nano/popularmovies/MovieAdapter.java
@@ -5,36 +5,50 @@ import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MovieAdapter extends CursorAdapter{
+import com.samutamm.nano.popularmovies.domain.Movie;
+import com.squareup.picasso.Picasso;
 
-    public MovieAdapter(Context context, Cursor c, int flags) {
-        super(context, c, flags);
+import java.util.List;
+
+public class MovieAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private List<Movie> movieList;
+
+    public MovieAdapter(Context c, List<Movie> movies) {
+        mContext = c;
+        movieList = movies;
     }
 
-    @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        final View view = LayoutInflater.from(context).inflate(R.layout.list_movie_item, parent, false);
-        ViewHolder viewHolder = new ViewHolder(view);
-        view.setTag(viewHolder);
-        return view;
+    public int getCount() {
+        return movieList.size();
     }
 
-    @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-
+    public Object getItem(int position) {
+        return 0;
     }
 
-    public static class ViewHolder {
-        public final ImageView iconView;
-        public final TextView nameView;
+    public long getItemId(int position) {
+        return 0;
+    }
 
-        public ViewHolder(View view) {
-            iconView = (ImageView)view.findViewById(R.id.list_movie_image);
-            nameView = (TextView)view.findViewById(R.id.list_movie_name);
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+        } else {
+            imageView = (ImageView) convertView;
         }
+        String imageUrl = "http://image.tmdb.org/t/p/w92/" + movieList.get(position).getPoster_path();
+        Picasso.with(mContext)
+                .load(imageUrl)
+                .into(imageView);
+        return imageView;
     }
 }
--- app/src/main/java/com/samutamm/nano/popularmovies/SearchFragment.java
+++ app/src/main/java/com/samutamm/nano/popularmovies/SearchFragment.java
@@ -1,32 +1,19 @@
 package com.samutamm.nano.popularmovies;
 
-import android.app.Activity;
 import android.app.Fragment;
-import android.database.Cursor;
-import android.database.MatrixCursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.GridView;
 
 import com.samutamm.nano.popularmovies.domain.Movie;
 import com.samutamm.nano.popularmovies.sync.HttpClient;
 import com.samutamm.nano.popularmovies.sync.Parser;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,20 +21,20 @@ public class SearchFragment extends Fragment implements AdapterView.OnItemSelect
     public final String LOG_TAG = SearchFragment.class.getSimpleName();
 
     private MovieAdapter movieAdapter;
+    private List<Movie> movieList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        List<String> list = new ArrayList<>();
-        movieAdapter = new MovieAdapter(getActivity(), null, 0);//new ArrayAdapter<String>(getActivity(),
-                //android.R.layout.simple_list_item_1, android.R.id.text1, list);
+        movieList = new ArrayList<>();
+        movieAdapter = new MovieAdapter(getActivity(), movieList);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.movie_list, container, false);
-        ListView movieList = (ListView)rootView.findViewById(R.id.movieList);
-        movieList.setAdapter(movieAdapter);
+        View rootView = inflater.inflate(R.layout.search_fragment, container, false);
+        GridView gridView = (GridView)rootView.findViewById(R.id.movieGrid);
+        gridView.setAdapter(movieAdapter);
         return rootView;
     }
 
@@ -69,7 +56,7 @@ public class SearchFragment extends Fragment implements AdapterView.OnItemSelect
         System.out.println("NOTHONG SELECTED");
     }
 
-    private class FetchApi extends AsyncTask<String, String, String[][]> {
+    private class FetchApi extends AsyncTask<String, String, Movie[]> {
 
         private  HttpClient client;
         private Parser parser;
@@ -81,7 +68,7 @@ public class SearchFragment extends Fragment implements AdapterView.OnItemSelect
         }
 
         @Override
-        protected String[][] doInBackground(String... strings) {
+        protected Movie[] doInBackground(String... strings) {
             final String json = client.fetchApi(strings[0]);
             List<Movie> movies = parser.parseJson(json);
 
@@ -91,18 +78,17 @@ public class SearchFragment extends Fragment implements AdapterView.OnItemSelect
                 String[] row = new String[]{i + 1+"", movie.getTitle(), movie.getPoster_path()};
                 mArray[i] = row;
             }
-            return mArray;
+            return movies.toArray(new Movie[movies.size()]);
         }
 
         @Override
-        protected void onPostExecute(String[][] results) {
+        protected void onPostExecute(Movie[] results) {
             if (results != null) {
-                String[] columns = new String[]{"_id", "name", "image"};
-                MatrixCursor cursor = new MatrixCursor(columns);
+                movieList.clear();
                 for (int i = 0; i < results.length; i++){
-                    cursor.addRow(results[i]);
+                    movieList.add(results[i]);
                 }
-                movieAdapter.swapCursor(cursor);
+                movieAdapter.notifyDataSetChanged();
             }
         }
     }
